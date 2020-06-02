41a9e90320ebfdad06e270a297f6971c7829fe45
Robert-Dobson <43008203+Robert-Dobson@users.noreply.github.com>
2020-06-02 17:39:11 +0100

    V2.5
    Added truth table generator which generates a string which represents the truth table. Added feature for first four switches be A, B, C, D

--- The Boolean Algebra Tutor/Assets/Scripts/Sandbox/Logic Gates/Switch.cs
+++ The Boolean Algebra Tutor/Assets/Scripts/Sandbox/Logic Gates/Switch.cs
@@ -4,15 +4,23 @@ public class Switch : MonoBehaviour
 {
     private bool state;
     public GameObject outputNode;
+    static int numOfSwitches = 0;
+    public string letter;
+
+    //Sprites for each possible switch
+    public Sprite spriteA;
+    public Sprite spriteB;
+    public Sprite spriteC;
+    public Sprite spriteD;
    
    
-    private void OnTrue()
+    public void OnTrue()
     {
         GetComponent<SpriteRenderer>().color = Color.green; //Change sprite to green
         outputNode.GetComponent<OutputNode>().OnTrue(); //Call OnTrue method on output node
     }
 
-    private void OnFalse()
+    public void OnFalse()
     {
         GetComponent<SpriteRenderer>().color = Color.red; //Change sprite to red
         outputNode.GetComponent<OutputNode>().OnFalse(); //Call OnFalse method on output node
@@ -38,11 +46,35 @@ public class Switch : MonoBehaviour
         if (Input.GetKeyDown("d")) //If user presses d whilst hovering over the switch destroy it
         {
             Destroy(gameObject);
+            numOfSwitches -= 1;
         }
     }
 
     public void Start()
     {
+        numOfSwitches += 1; //Increment number of switches (static variable)
+        if(numOfSwitches == 1) //If first switch make it switch A
+        {
+            letter = "A";
+            GetComponent<SpriteRenderer>().sprite = spriteA;
+        }
+        else if (numOfSwitches == 2) //If second switch make it switch B
+        {
+            letter = "B";
+            GetComponent<SpriteRenderer>().sprite = spriteB;
+        }
+        else if (numOfSwitches == 3) //If third switch make it switch C
+        {
+            letter = "C";
+            GetComponent<SpriteRenderer>().sprite = spriteC;
+        }
+        else if (numOfSwitches == 4) //If fourth switch make it switch D
+        {
+            letter = "D";
+            GetComponent<SpriteRenderer>().sprite = spriteD;
+        }
+
+
         //Call OnFalse on first initialization to fix white logic gates bug
         OnFalse();
     }
---
+++ The Boolean Algebra Tutor/Assets/Scripts/Sandbox/TruthTableGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TruthTableGenerator : MonoBehaviour
+{
+    private string truthTable;
+    public GameObject truthTableString;
+
+    public void GenerateTruthTable()
+    {
+        //Get all switch game objects and bulb game objects and assign to an array
+        GameObject[] switches = GameObject.FindGameObjectsWithTag("Switch");
+        GameObject[] bulb = GameObject.FindGameObjectsWithTag("Bulb");
+
+        //Validation
+        if (bulb.Length != 1) //Truth table only supports 1 bulb 
+        {
+            truthTableString.GetComponent<Text>().text = "Too many bulbs";
+        }
+        else if (switches.Length <= 1 || switches.Length > 4) //Truth Table only supports 2-4 switches
+        {
+            truthTableString.GetComponent<Text>().text = "There must only be 2 to 4 switches";
+        }
+        else
+        {
+            truthTable = ""; //Remove previous truth table data
+            //Sort the array so then switch A is first, switch B is second etc.
+            GameObject[] tempArray = switches; //Uses temp array so then I can adjust the real array values
+            foreach (GameObject aSwitch in tempArray)
+            {
+                if (aSwitch.GetComponent<Switch>().letter == "A")
+                {
+                    switches[0] = aSwitch;
+                }
+                else if (aSwitch.GetComponent<Switch>().letter == "B")
+                {
+                    switches[1] = aSwitch;
+                }
+                else if (aSwitch.GetComponent<Switch>().letter == "C")
+                {
+                    switches[2] = aSwitch;
+                }
+                else if (aSwitch.GetComponent<Switch>().letter == "D")
+                {
+                    switches[3] = aSwitch;
+                }
+            }
+
+
+            for (int i=0; i < Math.Pow(2, switches.Length) ; i++)
+            {
+                //Loop through every combination of inputs
+                string countString = Convert.ToString(i, 2); //Adds up in binary so then every possible set of inputs is covered (logic gates work in binary)
+                countString = countString.PadLeft(switches.Length, '0'); //Adds leading 0's so string is always switches.length long (represents each input)
+                for (int j=0; j < switches.Length; j++)
+                {
+                    //Access each input, gets the character in countString which represents the input's state and sets state appropriately
+                    char state = countString[j];
+                    if (state == '1')
+                    {
+                        switches[j].GetComponent<Switch>().OnTrue();
+                    }
+                    else
+                    {
+                        switches[j].GetComponent<Switch>().OnFalse();
+                    }
+                }
+
+                truthTable += Convert.ToString(Convert.ToInt32(bulb[0].GetComponent<Bulb>().state)); //Add bulb's state to Truth Table
+            }
+
+            truthTableString.GetComponent<Text>().text = truthTable;
+        }
+
+
+    }
+}
