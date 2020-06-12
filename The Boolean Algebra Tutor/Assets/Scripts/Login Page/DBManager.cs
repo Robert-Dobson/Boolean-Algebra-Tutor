0280d8c5e16b53b53ece72f5f916499652ebff7b
Robert-Dobson <43008203+Robert-Dobson@users.noreply.github.com>
2020-06-12 11:01:44 +0100

    V3.1
    Created Login System UI. Created a database with Accounts table. Added ability for user to enter their username and password and receive either correct or incorrect back. Added protections against SQL injection. Added SHA256 hashed passwords.

---
+++ The Boolean Algebra Tutor/Assets/Scripts/Login Page/DBManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Data.SQLite;
+using System;
+
+public class DBManager : MonoBehaviour
+{
+    static private string connectionString; //Stores the location of the database
+    static SQLiteConnection databaseConnection; //SQLite connection to databse
+    static SQLiteCommand databaseCMD; //SQLite Commands
+
+    private void Start()
+    {
+        //Set connection string to location of database and create new SQLite connection to it
+        connectionString = "URI=file:" + Application.dataPath + "/BooleanAlgebraTutorDB.db; Version=3";
+        databaseConnection = new SQLiteConnection(connectionString);
+    }
+
+
+    public int CheckCredentials(string username, string password)
+    {
+        //Create SQL command to fetch any account ID which has the entered username and password.
+        SQLiteDataReader dbDataReader;  
+        databaseCMD = databaseConnection.CreateCommand();
+        databaseCMD.CommandText = 
+        @"
+            SELECT AccountID 
+            FROM Accounts 
+            WHERE Username= $username and Password= $password 
+        ";
+        // Set username and password parameters (to protect against SQL injection) to username and password entered
+        databaseCMD.Parameters.AddWithValue("$username", username);
+        databaseCMD.Parameters.AddWithValue("$password", password);
+        int AccountID = -1; //Default -1 for when no records are found.
+       
+        //Open Database connection and execute the SQL command then close the connection
+        databaseConnection.Open();
+        dbDataReader = databaseCMD.ExecuteReader();
+        while (dbDataReader.Read()) //Every iteration is for each record found
+        {
+            AccountID = Convert.ToInt32(dbDataReader["AccountID"]);
+        }
+        dbDataReader.Close();
+        databaseConnection.Close();
+        return AccountID;
+    }
+}
---
+++ The Boolean Algebra Tutor/Assets/Scripts/Login Page/LoginManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class LoginManager : MonoBehaviour
+{
+    //References to username and password text boxes
+    public GameObject usernameTextBox;
+    public GameObject passwordTextBox;
+
+    //Reference to error label;
+    public GameObject errorLabel;
+
+    public void OnLogin()
+    {
+        // Get the entered username and password.
+        string enteredUsername = usernameTextBox.GetComponent<TMP_InputField>().text;
+        string enteredPassword = passwordTextBox.GetComponent<TMP_InputField>().text;
+
+        //Convert entered Password to hash for comparision in database
+        enteredPassword = Sha256(enteredPassword);
+        
+        //Get the associated accountID for these entered credentials (or -1 if the credentials is incorrect)
+        int accountID = GetComponent<DBManager>().CheckCredentials(enteredUsername, enteredPassword);
+        
+        // If correct credentials then call CorrectCredentials() else call IncorrectCredentials()
+        if (accountID != -1)
+        {
+            CorrectCredentials();
+        }
+        else
+        {
+            IncorrectCredentials();
+        }
+
+    }
+
+    public string Sha256(string plainText)
+    {
+        //Create a hash
+        using (SHA256 hasher = SHA256.Create())
+        {
+            //Compute the hash, store as bytes
+            byte[] bytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+
+            // Convert byte aray to a string
+            string hash = "";
+            foreach (byte aByte in bytes)
+            {
+                hash += aByte.ToString("x2");
+            }
+            return hash;
+        }
+        
+    }
+
+    public void CorrectCredentials()
+    {
+        errorLabel.GetComponent<TextMeshProUGUI>().text = "Correct!";
+    }
+
+    public void IncorrectCredentials()
+    {
+        errorLabel.GetComponent<TextMeshProUGUI>().text = "Incorrect!";
+
+    }
+}
